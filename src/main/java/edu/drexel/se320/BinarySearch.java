fc57373acb57f087061d4b9cd00891aa0005e213
Samii Shabuse <sshabuse@gmail.com>
2025-10-15 19:19:54 -0400

    make base binary search

--- src/main/java/edu/drexel/se320/BinarySearch.java
+++ src/main/java/edu/drexel/se320/BinarySearch.java
@@ -11,6 +11,21 @@ public class BinarySearch {
         int left = 0;
         int right = array.length - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int compare =  elem.compareTo(array[mid]);
+
+            if  (compare == 0) {
+                return mid; // Element was found
+            }
+            else if (compare < 0) {
+                left = mid + 1; // Search the left half
+            }
+            else if (compare > 0) {
+                right = mid - 1; // search the right half
+            }
+
+            throw new NoSuchElementException("Element was not in array.");
 
         }
     }
